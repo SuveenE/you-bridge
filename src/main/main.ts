99c7940fbaef6b1603c39938c3ae605234abf0e8
SuveenE <suveen.te1@gmail.com>
2025-04-26 00:05:01 -0700

    Change autoDownload to true

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,7 +11,6 @@
 import path from 'path';
 import { app, BrowserWindow, shell, ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
-import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 import setupAppleNotesHandlers from './appleNotes';
@@ -19,7 +18,7 @@ import setupAppleNotesHandlers from './appleNotes';
 class AppUpdater {
   constructor() {
     // Disable automatic downloading of updates
-    autoUpdater.autoDownload = false;
+    autoUpdater.autoDownload = true;
     // Enable automatic installation of updates on the next computer restart
     autoUpdater.autoInstallOnAppQuit = true;
 
